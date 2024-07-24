1eb5134ec509e9ce8efd675ad65ac05adbbf0ce6
camaya21 <cindymamaya21@gmail.com>
2024-07-24 13:49:41 -0400

    Methods for GamePanel

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -1,7 +1,43 @@
-import javax.swing.JPanel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
-    
+    GamePanel(){
+
+    }
+    public void StartGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
 }
